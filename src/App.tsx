2cd138d69c694ce7a2806dfcb4aa42438ed585e1
Omar Abdallah <oamas857@gmail.com>
2025-09-14 10:38:48 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import {  useTheme } from "./contexts/Theme/ThemeContext"
 import { Theme } from "./types/theme";
 function App() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { theme, setMyTheme } = useTheme();
 
-  setMyTheme(Theme.Light);
+  setMyTheme(Theme.Dark);
 
   return (
     <div className=" bg-primary dark:bg-primary-dark 100 min-h-screen flex items-center justify-center" >
