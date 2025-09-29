b5256332a74295a83705ef47fe43de6d70fa4b34
Omar Abdallah <oamas857@gmail.com>
2025-09-29 23:03:17 +0300

    small bugs fixed

--- src/layout/TraineePageLayout.tsx
+++ src/layout/TraineePageLayout.tsx
@@ -8,11 +8,12 @@ import Profile from "../components/Profile";
 export default function TraineePageLayout() {
     const [hideMenu, setHideMenu] = useState(true); //true by default
     return <div className="bg-[#F6F8FB] dark:bg-secondary-dark w-full min-h-dvh">
-        <Navbar buttonLabel="omar" isLandingPage={false}/>
+        <Navbar buttonLabel="Omar Ahmed" />
         {/* // breakpoint is md */}
         <div className="flex mx-auto justify-center gap-[2vw] px-[5vw]">
             <OptionsMenu hideMenu={hideMenu} setHideMenu={setHideMenu}/>
             {hideMenu && (<Routes>
+                {/* <Route path="/" element={<Navigate to={"/dashboard"} />}/> */}
                 <Route path="/dashboard" element={<Dashboard />}/>
                 <Route path="/profile" element={<Profile />}/>
                 <Route path="*" element={<Navigate to={'/'} />}/>
