2a7b5fbd6f962d663789902d16f4c77208005f85
Omar Abdallah <oamas857@gmail.com>
2025-11-15 20:23:22 +0200

    working on the nutrition page

--- src/components/MealPlans.tsx
+++ src/components/MealPlans.tsx
@@ -1,12 +1,48 @@
-import { mealPlans } from "../types/meals";
-import { PlanCard } from "./PlanCard";
+import { ChefHat, DumbbellIcon } from "lucide-react";
+import { mealPlans } from "../types/mealPlansData";
+import { useSelector } from "react-redux";
+import type { RootState } from "../store/store";
 
 export function MealPlans() {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {/* {mealPlans.map((plan, idx) => (
-        <PlanCard key={idx} plan={plan} />
-      ))} */}
-    </div>
-  );
+  const authData = useSelector((state: RootState) => state.Authantication);
+  const userData = authData.user;
+
+  return <div className=" grid grid-cols-1 sm:grid-cols-2 gap-4 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+    {Object.keys(mealPlans).map((planName, idx) => {
+      const planInfo = mealPlans[planName];
+      const isCurrentPlan = planName == userData?.nutritionData?.selectedPlan;
+      return (
+        <div key={idx} className="text-text bg-[#FEFEFE] border w-max border-gray-200 rounded-lg p-6
+      flex flex-col justify-between max-w-70 xl:max-w-full">
+          <div>
+            <DumbbellIcon />
+
+            <h3 className="mt-3 font-medium text-slate-700">{planName}</h3>
+            <p className="pb">{planInfo.description}</p>
+          </div>
+          <div className="space-y-1 mt-4">
+            <div className="flex justify-between"><p className="font-semibold">Duration: </p> <p>{planInfo.dailyCalories}cal</p></div>
+            <div className="flex justify-between"><p className="font-semibold">Frequncy: </p> <p>{planInfo.fat}</p></div>
+            {/* <div className="flex justify-between"><p className="font-semibold">Difficulty: </p> <p className="border-1 rounded-md font-bold h-fit text-[12px] text-[#7F7F7F] text-center px-1 bg-[#FBFBFB]">{planInfo.difficulty}</p></div> */}
+            <button className={`bg-black ${isCurrentPlan ? "text-[#65656F]" : "text-white"} text-center w-full rounded-sm py-1 cursor-pointer hover:bg-slate-700`}
+              disabled={isCurrentPlan}
+              onClick={() => {
+
+              }}
+            >{isCurrentPlan ? "Current Plan" : "Start Workout"}</button>
+          </div>
+        </div>
+      )
+    })}
+
+  </div>;
+
+
+  // return (
+  //   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+  //     {/* {mealPlans.map((plan, idx) => (
+  //       <PlanCard key={idx} plan={plan} />
+  //     ))} */}
+  //   </div>
+  // );
 }
